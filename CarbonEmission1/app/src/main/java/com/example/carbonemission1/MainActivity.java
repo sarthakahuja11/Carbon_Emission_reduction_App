89dc49aebd40f3af65b915179b21d18324ad1526
rahulkstk <84446317+rahulkstk@users.noreply.github.com>
2021-11-28 23:45:05 +0000

    Implementation 2.0

---
+++ CarbonEmission1/app/src/main/java/com/example/carbonemission1/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.carbonemission1;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.fragment.app.Fragment;
+import android.content.Context;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+
+    ViewPager mSlideViewPager;
+    LinearLayout mDotLayout;
+    Button skipbtn;
+
+    TextView[] dots;
+    ViewPagerAdapter viewPagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        skipbtn = findViewById(R.id.skipButton);
+
+
+
+
+        skipbtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v){
+
+
+                Intent i = new Intent(MainActivity.this,mainscreen.class);
+                startActivity(i);
+                finish();
+
+
+            }
+        });
+
+        mSlideViewPager = (ViewPager) findViewById(R.id.slideViewPager);
+        mDotLayout = (LinearLayout) findViewById(R.id.indicator_layout);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+
+        mSlideViewPager.setAdapter(viewPagerAdapter);
+
+        setUpindicator(0);
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+    public void setUpindicator(int position){
+
+        dots = new TextView[3];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i< dots.length ; i++){
+
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.inactive, getApplicationContext().getTheme()));
+            mDotLayout.addView(dots[i]);
+
+        }
+
+        dots[position].setTextColor(getResources().getColor(R.color.active,getApplicationContext().getTheme()));
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.SimpleOnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels){
+
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public void onPageSelected(int position) {
+
+            setUpindicator(position);
+
+
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+
+        }
+
+    };
+}
