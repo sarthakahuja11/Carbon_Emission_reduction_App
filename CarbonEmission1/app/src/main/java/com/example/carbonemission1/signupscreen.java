1ca276da33cf52239cc81189d2928495cee6db1e
rahulkstk <84446317+rahulkstk@users.noreply.github.com>
2021-11-28 18:45:01 +0000

    Implementation
    implemented onboarding and login screens

---
+++ CarbonEmission1/app/src/main/java/com/example/carbonemission1/signupscreen.java
@@ -0,0 +1,31 @@
+package com.example.carbonemission1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class signupscreen extends AppCompatActivity {
+
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signupscreen);
+
+        button = (Button) findViewById(R.id.signupbtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(signupscreen.this, loginscreen.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
